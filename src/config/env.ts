741b134ec0e77ed30fcbdc510cccc4ca3acdcf0e
Sarajit Mandal <bdsarajit499@gmail.com>
2025-06-20 20:02:43 +0600

    set test lint

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,10 +1,10 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const env = {
     port : process.env.PORT,
     mongodb_url : process.env.MONGODB_URL,
-}
+};
 
 export default env;
