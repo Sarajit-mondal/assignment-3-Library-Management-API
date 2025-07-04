a076b119479486e022c5a08dbbe7e9318c31b29c
sarajit mondal <bdsarajit499@gmail.com>
2025-07-04 08:29:33 +0600

    book mobile segma updated

--- src/schemas/book.schema.ts
+++ src/schemas/book.schema.ts
@@ -9,18 +9,23 @@ const genreEnum = [
   "HISTORY",
   "BIOGRAPHY",
   "FANTASY"
-] as const;
+];
  export const bookSchema  = z.object({
   title: z.string().min(1, "Title is required"),
   author: z.string().min(1, "Author is required"),
  genre: z.string().refine(
-    (val) => genreEnum.includes(val as any),
+    (val) => genreEnum.includes(val),
     { message: "Genre must be one of: FICTION, NON_FICTION, SCIENCE, HISTORY, BIOGRAPHY, FANTASY" }
   ),
   isbn: z.string().min(10).max(17), // You can refine with regex if needed
   description: z.string().min(10, "Description should be at least 10 characters"),
-  copies: z.number().int().positive(),
-  available: z.boolean(),
+  copies: z
+    .number()
+    .int()
+    .min(0, { message: "Copies can’t be negative" }),   // or .nonnegative()
+
+  // if the field is missing or undefined, it becomes true
+  available: z.boolean().default(true).optional(),
 });
 
 
